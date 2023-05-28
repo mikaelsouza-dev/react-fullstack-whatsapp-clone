c3c17c8655fa9ff78b019732d80354d9d7371beb
unknown <mikael.silva62@aluno.ce.gov.br>
2023-05-28 19:14:47 -0300

    initial configs

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+        ...
+      </div>
+      <div className="contentArea">
+        ...
+      </div>
+    </div>
+  )
+}
+
+export default App
