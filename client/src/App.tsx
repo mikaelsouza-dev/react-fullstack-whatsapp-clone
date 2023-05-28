cd421d584afb90bfa4aa82df15d9f9edc9ec0279
unknown <mikael.silva62@aluno.ce.gov.br>
2023-05-28 19:59:13 -0300

    right-area style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
-import './App.css'
+import './App.scss'
+import { MdDonutLarge, MdChat, MdMoreVert } from 'react-icons/md'
+
 
 function App() {
 
   return (
     <div className="app-window">
       <div className="sidebar">
-        ...
+        <header>
+          <img className='header--avatar' src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <MdDonutLarge className='icon' />
+            </div>
+            <div className="header--btn">
+              <MdChat />
+            </div>
+            <div className="header--btn">
+              <MdMoreVert />
+            </div>
+          </div>
+        </header>
+        <div className="search">
+          ...
+        </div>
+        <div className="chatlist">
+          ...
+        </div>
       </div>
       <div className="contentArea">
         ...
