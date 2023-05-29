6d9c339ceee3fe0aa56e648ecac50a24887eea27
unknown <mikael.silva62@aluno.ce.gov.br>
2023-05-28 21:21:33 -0300

    sidebar search styled

---
+++ client/components/ChatListItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const ChatListItem = () => {
+
+
+    return (
+        <div>...</div>
+    )
+}
+
+export default ChatListItem
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.scss'
-import { MdDonutLarge, MdChat, MdMoreVert } from 'react-icons/md'
-
+import React, { useState } from 'react'
+import { MdDonutLarge, MdChat, MdMoreVert, MdSearch } from 'react-icons/md'
+import ChatListItem from '../components/ChatListItem'
 
 function App() {
+  const [chatlist, setChatlist] = useState([{}, {},{}, {}])
 
   return (
     <div className="app-window">
       <div className="sidebar">
+        
         <header>
           <img className='header--avatar' src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
           <div className="header--buttons">
@@ -22,10 +25,17 @@ function App() {
           </div>
         </header>
         <div className="search">
-          ...
+          <div className="search--input">
+            <MdSearch className='icon' />
+            <input type="search" placeholder='Procurar ou começar uma nova conversa.' />
+          </div>
         </div>
         <div className="chatlist">
-          ...
+          {chatlist.map((item, key) => (
+            <ChatListItem
+            key={key}
+            />
+          ))}
         </div>
       </div>
       <div className="contentArea">
