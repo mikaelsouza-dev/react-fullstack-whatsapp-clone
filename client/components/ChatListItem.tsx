cbd9f4b451a495b383a06b0c2cb975abe53e0a71
unknown <mikael.silva62@aluno.ce.gov.br>
2023-05-28 21:30:47 -0300

    chatlistitem added

--- client/components/ChatListItem.tsx
+++ client/components/ChatListItem.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
 const ChatListItem = () => {
 
 
     return (
-        <div>...</div>
+        <div className="chatListItem">
+            <img className="avatar" src="" alt="" />
+            <div className="lines">
+                <div className="line">
+                    <div className="name">Mikael Souza</div>
+                    <div className="date">19:00</div>
+                </div>
+                <div className="line">
+                    <div className="lasMsg">
+                        <p>Opa, tudo bem?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
