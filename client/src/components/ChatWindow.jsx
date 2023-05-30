6817b4f668e6199032356b596d4b6d5173b5b130
unknown <mikael.silva62@aluno.ce.gov.br>
2023-05-30 20:55:52 -0300

    emoji area finalized

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -1,8 +1,23 @@
 import './ChatWindow.scss';
-import {MdSearch, MdAttachFile, MdMoreVert, MdInsertEmoticon, MdClose, MdSend, MdMic} from 'react-icons/md'
+import { MdSearch, MdAttachFile, MdMoreVert, MdInsertEmoticon, MdClose, MdSend, MdMic } from 'react-icons/md'
+import EmojiPicker from 'emoji-picker-react';
+import { useState } from 'react';
 
-function ChatWindow( activeChat ) {
-    console.log(activeChat)
+function ChatWindow(activeChat) {
+  
+  const [emojiOpen, setEmojiOpen] = useState(false);
+    
+  const handleEmojiClick = () => {
+    console.log('t')
+  }
+
+  const handleOpenEmoji = () => {
+    setEmojiOpen(true)
+  }
+  const handleCloseEmoji = () => {
+    setEmojiOpen(false)
+  }
+
   return (
     <div className="chatWindow">
         <div className="header">
@@ -19,11 +34,22 @@ function ChatWindow( activeChat ) {
               </div>
             </div>
         </div>
-        <div className="body"></div>
+      <div className="body"></div>
+      
+      <div className="emojiarea" style={{height: emojiOpen? '350px' : '0'}}>
+        <EmojiPicker
+          width={'auto'}
+          onEmojiClick={handleEmojiClick}
+        />
+      </div>
+
       <div className="footer">
         <div className="pre">
           <div className="btn">
-            <MdInsertEmoticon />
+            <MdClose onClick={handleCloseEmoji} style={{width: emojiOpen? 40 : 0}} />
+          </div>
+          <div className="btn" onClick={handleOpenEmoji}>
+            <MdInsertEmoticon style={{color: emojiOpen? '#009688' : '#919191'}} />
           </div>
         </div>
         <div className="inputarea">
