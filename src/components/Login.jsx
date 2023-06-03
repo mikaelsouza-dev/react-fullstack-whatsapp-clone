977325e86347164b5896ace9d691aa141d7b7510
unknown <mikael.silva62@aluno.ce.gov.br>
2023-06-03 18:51:02 -0300

    add other login methods

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Api from '../api/Api';
 import './Login.css'
+import { FaGithub, FaGoogle, FaMicrosoft, FaTwitter} from 'react-icons/fa'
 
 const Login = ({onReceive}) => {
     const handleGoogleLogin = async () => {
@@ -12,9 +13,23 @@ const Login = ({onReceive}) => {
         }
     }
 
+    const handleTwitterLogin = async () => {
+        let result = await Api.ttPopup();
+        if (result) {
+            onReceive(result.user)
+        } else {
+            alert('deu ruim')
+        }
+    }
+
   return (
       <div className='login'>
-          <button onClick={handleGoogleLogin}>Logar com Google</button>
+          <h1>Acesse sua conta</h1>
+          <p>Bem-vindo à página de login! Para acessar sua conta, insira suas informações de login nos campos abaixo. Certifique-se de digitar corretamente seu nome de usuário e senha. Se ainda não possui uma conta, clique no link 'Registrar' para criar uma.</p>
+          <button onClick={handleGoogleLogin}><FaGoogle className='socialIcon' /> Entrar com Google</button>
+          <button onClick={handleTwitterLogin}><FaTwitter className='socialIcon' /> Entrar com Twitter</button>
+          <button onClick={handleGoogleLogin}><FaGithub className='socialIcon' /> Entrar com GitHub</button>
+          <button onClick={handleGoogleLogin}><FaMicrosoft className='socialIcon' /> Entrar com Microsoft</button>
     </div>
   )
 }
